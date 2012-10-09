c29650f7517cc6dea86f95c0d67dcf65fdcf3263
mateusweb <mateusweb@gmail.com>
2012-10-08 21:40:33 -0300

    ajustes de espaçamentos

--- v6.0.0.0/NadebLive/CropImages.php
+++ v6.0.0.0/NadebLive/CropImages.php
@@ -16,48 +16,48 @@ class CropImages
 	private $final_height;
 	private $bgRGB;
 	public $fixedTop;
-		
+
 	public function __construct()
 	{
 		$this->fixedTop  = false;
 		$this->bgRGB     = array( 220, 230, 240 );
-		
+
 	}
-	
+
 	public function set_patch($_value)
 	{
 		$this->patch = $_value;
-		
+
 		return $this;
 	}
-	
+
 	public function set_file($_value)
 	{
 		$this->file = $_value;
-		
+
 		return $this;
 	}
-	
+
 	public function set_newName($_value)
 	{
 		$this->new_name = __ROOT__ . '/' . $this->patch . '/' . $_value;
-		
+
 		return $this;
 	}
-	
+
 	public function set_newSize($_w = null, $_h = null)
 	{
 		$path = __ROOT__ . '/' . $this->patch .'/'. $this->file;
 		$path = preg_replace('/\/\/+/','/',$path);
 		$tmImg = getimagesize( $path );
 		$_h = ($_h) ? $_h : round( ($tmImg[1] / $tmImg[0]) * $_w );
-		
+
 		$this->new_width  = $_w;
 		$this->new_height = $_h;
-		
+
 		return $this;
 	}
-	
+
 	public function create()
 	{
 	    $file = __ROOT__ . '/' . $this->patch . '/' . $this->file;
@@ -89,27 +89,27 @@ class CropImages
 //	    	$this->img = imagecreatefromgif("http://dummyimage.com/" . $this->new_width . "x" . $this->new_height);
 	    	$this->img = @imagecreatefromgif("http://dummyimage.com/1x1");
 	    }
-	    
+
 		$this->atual_width  = @imagesx( $this->img );
 		$this->atual_height = @imagesy( $this->img );
-	    
+
 		$this->final_width  = $this->new_width;
 		$this->final_height = $this->new_height;
-		
+
 		return $this;
-	    
+
 	}
 
 	public function crop_by_width()
 	{
 		$this->final_width  = $this->new_width;
 		$this->final_height = $this->new_height;
-		
+
 		$this->new_width = floor( $this->atual_width * ( $this->new_height / $this->atual_height ) );
-		
+
 		$this->positY = ($this->fixedTop) ? 0 : ($this->final_width - $this->new_width) / 2;
 		$this->positX = ($this->final_height - $this->new_height) / 2;
-		
+
 		return $this;
 	}
 
@@ -117,12 +117,12 @@ class CropImages
 	{
 		$this->final_width  = $this->new_width;
 		$this->final_height = $this->new_height;
-		
+
 		$this->new_height = floor( $this->atual_height * ( $this->new_width / $this->atual_width ) );
-		
+
 		$this->positX = ($this->fixedTop) ? 0 : ($this->final_height - $this->new_height) / 2;
 		$this->positY = ($this->final_width - $this->new_width) / 2;
-		
+
 		return $this;
 	}
 
@@ -132,29 +132,28 @@ class CropImages
 		{
 			$this->crop_by_height();
 		}
-		
+
 		if($this->atual_width > $this->atual_height)
 		{
 			$this->crop_by_width();
 		}
-		
+
 		if($this->atual_width == $this->atual_height)
 		{
 			$this->new_width   = $this->final_width;
 			$this->new_height  = $this->final_height;
-			
+
 			$this->positY =($this->fixedTop) ? 0 : ($this->final_width - $this->new_width) / 2;
 			$this->positX = ($this->final_height - $this->new_height) / 2;
 		}
-		
+
 		return $this;
 	}
 
 	public function set_bgRGB($red, $green, $blue)
 	{
-		
 	    $this->bgRGB = array( $red, $green, $blue );
-	    
+
 		return $this;
 	}
 
@@ -162,20 +161,20 @@ class CropImages
 	{
 		$tmp_img = imagecreatetruecolor( $this->final_width,$this->final_height );
 		$white   = imagecolorallocate( $tmp_img, $this->bgRGB[0], $this->bgRGB[1], $this->bgRGB[2] );
-		
+
 		imagefilledrectangle($tmp_img, 0, 0, $this->final_width, $this->final_height, $white);
-		@imagecopyresized( 
-			$tmp_img, 
-			$this->img, 
-			$this->positY, $this->positX, 0, 0, 
+		@imagecopyresized(
+			$tmp_img,
+			$this->img,
+			$this->positY, $this->positX, 0, 0,
 			$this->new_width,
-			$this->new_height, 
-			$this->atual_width, 
+			$this->new_height,
+			$this->atual_width,
 			$this->atual_height
 		);
-				
+
 		imagepng($tmp_img, $this->new_name);
 		imagedestroy($tmp_img);
 	}
-	
+
 }
